34529d549cd945d8b5aaf69ce66fecb1b1b349ff
David Hewson <dev@daveid.co.uk>
2018-03-02 21:36:59 +0000

    hey let's decode things

--- src/lib.rs
+++ src/lib.rs
@@ -112,7 +112,10 @@ impl Board {
         }
 
         let spares = self.spares.iter().map(encode_spare);
-        let flower = self.flower.iter().map(encode_flower);
+        let flower = [&self.flower];
+        let flower = flower
+            .iter()
+            .map(|f| f.as_ref().map(encode_flower).unwrap_or_else(String::new));
         let places = self.places.iter().map(encode_place);
         let piles = self.piles.iter().map(encode_pile);
 
@@ -120,9 +123,110 @@ impl Board {
             .chain(flower)
             .chain(places)
             .chain(piles)
-            .fold(String::new(), |a, c| format!("{};{}", a, c))
+            .collect::<Vec<_>>()
+            .join(";")
+    }
+
+    pub fn decode(s: &str) -> Result<Board, &'static str> {
+        let split = s.split(";").collect::<Vec<_>>();
+
+        if split.len() != 15 {
+            Err("split.length() != 15")?
+        }
+
+        fn decode_card(s: &str) -> Result<Card, &'static str> {
+            if s.len() != 2 {
+                Err("bad length of card")?;
+            }
+            if s == "ff" {
+                return Ok(Card::Flower(FlowerCard));
+            }
+
+            let suit = match s.as_bytes()[0] {
+                b'b' => Suit::Black,
+                b'g' => Suit::Green,
+                b'r' => Suit::Red,
+                b => Err("bad suit")?,
+            };
+
+            Ok(match s.as_bytes()[1] {
+                v @ b'1'...b'9' => Card::Value(ValueCard(suit, v - b'0')),
+                b'D' => Card::Dragon(suit),
+                _ => Err("bad value")?,
+            })
+        }
+
+        fn decode_spare(s: &str) -> Result<Spare, &'static str> {
+            Ok(match s {
+                "" => Spare::Empty,
+                "bDbDbDbD" => Spare::DragonStack(Suit::Black),
+                "gDgDgDgD" => Spare::DragonStack(Suit::Green),
+                "rDrDrDrD" => Spare::DragonStack(Suit::Red),
+                _ => Spare::Card(decode_card(s)?),
+            })
+        }
+
+        fn decode_flower(s: &str) -> Result<Option<FlowerCard>, &'static str> {
+            Ok(match s {
+                "" => None,
+                "ff" => Some(FlowerCard),
+                _ => Err("that isn't a flower")?,
+            })
+        }
+
+        fn decode_place(s: &str) -> Result<Place, &'static str> {
+            Ok(match s {
+                "" => Place::Empty,
+                _ => match decode_card(s)? {
+                    Card::Dragon(_) => Err("Dragons can't placed")?,
+                    Card::Flower(_) => Err("Flowers can't placed")?,
+                    Card::Value(v) => Place::Card(v),
+                },
+            })
+        }
+
+        fn decode_pile(s: &str) -> Result<SmallVec<[Card; 13]>, &'static str> {
+            if s.len() % 2 != 0 {
+                Err("Piles must be even length")?
+            }
+
+            let mut pile = SmallVec::new();
+
+            for i in 0..s.len() / 2 {
+                let start = i * 2;
+                let st = &s[start..start + 2];
+                pile.push(decode_card(st)?)
+            }
+
+            Ok(pile)
+        }
+
+        Ok(Board {
+            spares: [
+                decode_spare(split[0])?,
+                decode_spare(split[1])?,
+                decode_spare(split[2])?,
+            ],
+            flower: decode_flower(split[3])?,
+            places: [
+                decode_place(split[4])?,
+                decode_place(split[5])?,
+                decode_place(split[6])?,
+            ],
+            piles: [
+                decode_pile(split[7])?,
+                decode_pile(split[8])?,
+                decode_pile(split[9])?,
+                decode_pile(split[10])?,
+                decode_pile(split[11])?,
+                decode_pile(split[12])?,
+                decode_pile(split[13])?,
+                decode_pile(split[14])?,
+            ],
+        })
     }
 }
+
 fn all_the_cards() -> Vec<Card> {
     let mut cards = Vec::with_capacity(1 + (9 + 4) * 3);
 
@@ -160,6 +264,8 @@ fn new_game() -> Board {
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
     fn there_are_so_many_cards() {
         assert_eq!(super::all_the_cards().len(), 40);
@@ -167,8 +273,25 @@ mod test {
 
     #[test]
     fn new_games_are_fun() {
-        let board = super::new_game();
-        println!("{}", board.encode());
+        let board = new_game();
+        let encoded = board.encode();
+        std::thread::spawn(|| {});
+        let decoded = Board::decode(&encoded);
+        assert_eq!(&decoded.unwrap(), &board);
         assert_eq!(board.flower, None);
+    }
+
+    #[test]
+    fn empty_board() {
+        let b = ";;;;;;;;;;;;;;";
+        let board = Board::decode(b).unwrap();
+        assert_eq!(b, board.encode());
+    }
+
+    #[test]
+    fn board_of_interest_0() {
+        let b = "gDgDgDgD;;;ff;;;;r6b5;r4g9bDr7;bDg5rDb6;b3g2r9b8;r3rDg8g6bD;rDg1b1r8b7;g7g4b4bDg3b2;b9r2r5r1rD";
+        let board = Board::decode(b).unwrap();
+        assert_eq!(b, board.encode());
     }
 }
