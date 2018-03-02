9c4d4b1db6d74646ca65b72f6537e95614f604ee
David Hewson <dev@daveid.co.uk>
2018-03-02 18:12:51 +0000

    encode a board

--- src/lib.rs
+++ src/lib.rs
@@ -60,8 +60,72 @@ impl Default for Board {
 }
 
 impl Board {
-    pub fn serialize(&self) -> String {
-        "".into()
+    pub fn encode(&self) -> String {
+        fn encode_suit (s: &Suit) -> &'static str {
+            match *s {
+                Suit::Black => "b",
+                Suit::Green => "g",
+                Suit::Red => "r",
+            }
+        }
+
+        fn encode_dragon_stack (suit: &Suit) -> String {
+            format!("{s}D{s}D{s}D{s}D", s = encode_suit(suit))
+        }
+
+        fn encode_flower (flower: &FlowerCard) -> String {
+            "ff".to_string()
+        }
+
+        fn encode_valuecard (vc: &ValueCard) -> String {
+            let &ValueCard(suit, value) = vc;
+            format!("{}{}", encode_suit(&suit), value)
+        }
+
+        fn encode_card (card: &Card) -> String {
+            match *card {
+                Card::Flower(ref f) => encode_flower(f),
+                Card::Dragon(ref suit) => format!("{}D", encode_suit(suit)),
+                Card::Value(ref v) => encode_valuecard(&v),
+            }
+        }
+
+        fn encode_spare (spare: &Spare) -> String {
+            match *spare {
+                Spare::Empty => String::new(),
+                Spare::Card(ref c) => encode_card(&c),
+                Spare::DragonStack(ref suit) => encode_dragon_stack(&suit),
+            }
+        }
+
+        fn encode_place (place: &Place) -> String {
+            match *place {
+                Place::Empty => String::new(),
+                Place::Card(ref vc) => encode_valuecard(&vc),
+            }            
+        }
+
+        fn encode_pile (pile: &SmallVec<[Card; 13]>) -> String {
+            pile.iter().map(encode_card).fold(String::new(), |a, c| a + &c)
+        }
+
+        [
+            encode_spare(&self.spares[0]),
+            encode_spare(&self.spares[1]),
+            encode_spare(&self.spares[2]),
+            self.flower.as_ref().map(encode_flower).unwrap_or_else(String::new),
+            encode_place(&self.places[0]),
+            encode_place(&self.places[1]),
+            encode_place(&self.places[2]),
+            encode_pile(&self.piles[0]),
+            encode_pile(&self.piles[1]),
+            encode_pile(&self.piles[2]),
+            encode_pile(&self.piles[3]),
+            encode_pile(&self.piles[4]),
+            encode_pile(&self.piles[5]),
+            encode_pile(&self.piles[6]),
+            encode_pile(&self.piles[7]),
+        ].join(";")
     }
 }
 
@@ -110,7 +174,7 @@ mod test {
     #[test]
     fn new_games_are_fun() {
         let board = super::new_game();
-        println!("{}", board.serialize());
+        println!("{}", board.encode());
         assert_eq!(board.flower, None);
     }
 }
