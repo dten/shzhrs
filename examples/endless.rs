ace67f05e8c86a2c5742fb4453282fe2a2459e41
David Hewson <dev@daveid.co.uk>
2018-03-04 11:25:16 +0000

    print basics instead of each board

--- examples/endless.rs
+++ examples/endless.rs
@@ -10,31 +10,29 @@ fn main() {
         .build_global()
         .unwrap();
 
-    let res = (0..100)
-        .collect::<Vec<_>>()
-        .par_iter()
+    (0..1000)
+        .into_par_iter()
         .map(|_| {
             let board = shzhrs::new_game();
             let encoded = board.encode();
             let start = Instant::now();
             match shzhrs::solve(&board) {
-                None => panic!("couldn't solve {}", encoded),
-                Some((path, cost)) => {
+                None => {
                     let time = Instant::now() - start;
-                    let boards = path.iter().map(|b| b.encode()).collect::<Vec<_>>();
-                    println!(
-                        "solved {:?}\n{:#?}\ncost: {} time: {:?}",
-                        encoded, boards, cost, time
-                    );
-                    return (time, encoded);
+                    return (false, time, encoded);
+                }
+                Some((_path, _cost)) => {
+                    let time = Instant::now() - start;
+                    //let boards = path.iter().map(|b| b.encode()).collect::<Vec<_>>();
+                    // println!(
+                    //     "solved {:?}\n{:#?}\ncost: {} time: {:?}",
+                    //     encoded, boards, cost, time
+                    // );
+                    return (true, time, encoded);
                 }
             }
         })
-        .max();
-
-    if let Some((longest_time, board)) = res {
-        println!("{} took the longest at {:?}", board, longest_time);
-    } else {
-        println!("Solved nothing");
-    }
+        .for_each(|(solved, time, encoded)| {
+            println!("Solved: {:?}, Time: {:?}, Board:{} ", solved, time, encoded);
+        });
 }
